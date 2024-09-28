f80801d57b39ee1f7b8e86278feea000a30ae9f3
Farkas Sandor <farkas.sandor2@gmail.com>
2024-09-28 15:11:05 +0200

    Initial commit

---
+++ src/test/java/com/dogadopt/dog_adopt/DogAdoptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogadopt.dog_adopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogAdoptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
