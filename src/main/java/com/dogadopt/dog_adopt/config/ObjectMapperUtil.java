3ffa71b78f3368a815f277ff785f6689b6fe3186
Farkas Sandor <farkas.sandor2@gmail.com>
2024-09-28 16:42:51 +0200

    ObjectMapperUtil class setup

---
+++ src/main/java/com/dogadopt/dog_adopt/config/ObjectMapperUtil.java
@@ -0,0 +1,26 @@
+package com.dogadopt.dog_adopt.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+import java.util.List;
+
+public class ObjectMapperUtil {
+
+    private static final ModelMapper modelMapper;
+
+    private ObjectMapperUtil() {}
+
+    static {
+        modelMapper = new ModelMapper();
+        modelMapper
+                .getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+    }
+
+    public static <D, T> List<D> mapAll(List<T> entities, Class<D> clazz) {
+        return entities.stream()
+                .map(e -> modelMapper.map(e, clazz))
+                .toList();
+    }
+}
