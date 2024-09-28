1c7f1ea87e9afaf9dc33f8c97526aa40cb28d8ec
Farkas Sandor <farkas.sandor2@gmail.com>
2024-09-28 17:59:14 +0200

    Dog validations

--- src/main/java/com/dogadopt/dog_adopt/domain/Dog.java
+++ src/main/java/com/dogadopt/dog_adopt/domain/Dog.java
@@ -5,10 +5,8 @@ import com.dogadopt.dog_adopt.domain.enums.dog.DogSize;
 import com.dogadopt.dog_adopt.domain.enums.dog.DonationGoal;
 import com.dogadopt.dog_adopt.domain.enums.dog.HealthStatus;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -25,25 +23,37 @@ public class Dog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
+    @Size(max = 50, message = "Dog name can be maximum {max} characters.")
     private String name;
 
+    @NonNull
     private Integer age;
 
+    @NonNull
+    @Enumerated(EnumType.STRING)
     private DogBreed breed;
 
+    @NonNull
+    @Enumerated(EnumType.STRING)
     private DogSize dogSize;
 
+    @NonNull
+    @Enumerated(EnumType.STRING)
     private HealthStatus healthStatus;
 
+    @Size(max = 500, message = "Description can have maximum {max} characters.")
     private String description;
 
     @OneToMany(mappedBy = "dog")
     private List<Image> images;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "shelter-id")
     private Shelter shelter;
 
+    @Enumerated(EnumType.STRING)
     private DonationGoal donationGoal;
 
     private int donationReceived;
