6330817ad59e1ea3395a1b4dd31fde6acabb981f
Farkas Sandor <farkas.sandor2@gmail.com>
2024-10-01 15:02:21 +0200

    new apikey for Cloudinary and added apikeyProperties

--- src/main/java/com/dogadopt/dog_adopt/config/CloudinaryConfig.java
+++ src/main/java/com/dogadopt/dog_adopt/config/CloudinaryConfig.java
@@ -5,8 +5,10 @@ import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:apikey.properties")
 public class CloudinaryConfig {
 
     @Value("${cloudinary.cloudName}")
